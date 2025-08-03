0ad3aa656ef37de12907d6d250361874de6be22f
Arjuna Marcelino <arjunamarcelino@gmail.com>
2025-08-03 23:22:00 +0700

    clean

--- app/Web3Provider.tsx
+++ app/Web3Provider.tsx
@@ -3,8 +3,8 @@
 import React from "react";
 import { Config, WagmiProvider } from "wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { XellarKitProvider, defaultConfig, darkTheme, lightTheme } from "@xellar/kit";
-import { base, baseSepolia, lisk, liskSepolia, polygonAmoy } from "viem/chains";
+import { XellarKitProvider, defaultConfig, lightTheme } from "@xellar/kit";
+import { base, baseSepolia, lisk, liskSepolia } from "viem/chains";
 
 const walletConnectProjectId = "YOUR_WALLET_CONNECT_PROJECT_ID";
 const xellarAppId = "eeb84396-c025-4cac-a75d-109fe5c2046d";
